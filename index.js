71b3b14c7c255c4da729a435c03650bd82ac615b
Tvoir <TommyVoir@yahoo.com>
2022-05-23 20:53:58 -0700

    added responses continue to down.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 //Your Code here
+
+
+let response = window.prompt('Do you head left or right?')
+
+console.log("the value of response is: ",response);
+
+
+if (response === "right"){
+    console.log("right was the response!");
+
+    let response2 = window.prompt('you selected right, go up or down?');
+
+    console.log(response2);
+
+    if(response2 === down){}
+
+
+} else if (response === "left"){
+    
+    console.log("left was the response!");
+
+}else{
+    
+    console.log("invalid response!")
+
+}
